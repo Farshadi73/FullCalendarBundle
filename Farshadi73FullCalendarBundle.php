27ebc0b2b25410362402e908c85f6d10018ddbdf
Farshad Asgharzade <Farshad_asgharzade@hotmail.com>
2018-07-17 21:26:51 +0430

    update bundle namespace

--- Farshadi73FullCalendarBundle.php
+++ Farshadi73FullCalendarBundle.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * This file is part of fullcalendarbundle
- * Copyrighted by Narmafzam (Farzam Webnegar Sivan Co.), info@narmafzam.com
+ * Copyrighted by Farshadi73 (Farzam Webnegar Sivan Co.), info@Farshadi73.com
  * Created by farshad
  * Date: 7/17/18
  */
 
-namespace Narmafzam\FullCalenderBundle;
+namespace Farshadi73\FullCalenderBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class Farshadi73FullCalendarBundle
- * @package Narmafzam\FullCalenderBundle
+ * @package Farshadi73\FullCalenderBundle
  */
 class Farshadi73FullCalendarBundle extends Bundle
 {
